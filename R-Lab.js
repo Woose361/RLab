4354dec9f49f3bcb1f77edfa82abbcb90e5b15c3
woose361 <devon.owusu361@gmail.com>
2024-11-15 20:57:53 -0500

    rlab

--- R-Lab.js
+++ R-Lab.js
@@ -4,7 +4,9 @@ const csvString = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,1
 let rows = csvString.split('\n'); // Split into rows
 
 // Step 2: Split each row into columns
-let twoDimensionalArray = rows.map(row => row.split(',')); // Create a 2D array
+let twoDimensionalArray = rows.map(row => row.split(',')); 
+
+// Create a 2D array
 
 // let objectKey =  csvString(row => row.split(" "))
 
@@ -22,8 +24,29 @@ let dataObjects = rows.slice(1).map(row => {
 });
 
 // Step 4: Log the array of objects
+
 dataObjects.pop();
 
 dataObjects.splice(1, 0, { id: "48", name: "Barry", occupation: "Runner", age: "25" });
 dataObjects.push(  { id: "7", name: "Bilbo", occupation: "None", age: "111" });
 console.log(dataObjects);
+
+
+
+let totalAge = 0; for (let i = 0; i < dataObjects.length; i++) {
+     totalAge += parseInt(dataObjects[i].age);
+};
+let averageAge = totalAge / dataObjects.length; console.log("Average Age:", averageAge);
+
+// Part 5
+// As a final task, transform the final set of data back into CSV format.
+// There are a number of ways to do this; be creative!
+// Once complete, be sure to submit your work according to the submission instructions at the beginning of this document.
+
+
+// This code uses the reduce function to iterate over the dataObjects array and build the CSV string in a csv file. 
+
+const csvContent = dataObjects.reduce((csv, obj) => 
+    { return csv + `${obj.id},${obj.name},${obj.occupation},${obj.age}\n`; }, 'ID,Name,Occupation,Age\n');
+
+console.log(csvContent);
