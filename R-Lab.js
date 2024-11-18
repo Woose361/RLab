b90d73866e4f12f05a5312bd9e770cb6bb2043fe
woose361 <devon.owusu361@gmail.com>
2024-11-18 10:13:36 -0500

    Final submission

--- R-Lab.js
+++ R-Lab.js
@@ -1,16 +1,17 @@
 const csvString = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
 
-// Step 1: Split the CSV string into rows
+// Part 1: Split the CSV string into rows
 let rows = csvString.split('\n'); // Split into rows
 
-// Step 2: Split each row into columns
+// Part 2: Split each row into columns
 let twoDimensionalArray = rows.map(row => row.split(',')); 
 
 // Create a 2D array
 
-// let objectKey =  csvString(row => row.split(" "))
 
 // Part 3: Transforming Data
+// For each object, we extract its properties and append them to csvContent in CSV format.
+
 let headers = rows[0].split(',').map(header => header.toLowerCase());
 
 
@@ -23,7 +24,7 @@ let dataObjects = rows.slice(1).map(row => {
     return obj;
 });
 
-// Step 4: Log the array of objects
+// Part 4: Log the array of objects
 
 dataObjects.pop();
 
@@ -36,7 +37,8 @@ console.log(dataObjects);
 let totalAge = 0; for (let i = 0; i < dataObjects.length; i++) {
      totalAge += parseInt(dataObjects[i].age);
 };
-let averageAge = totalAge / dataObjects.length; console.log("Average Age:", averageAge);
+let averageAge = totalAge / dataObjects.length; 
+console.log("Average Age:", averageAge);
 
 // Part 5
 // As a final task, transform the final set of data back into CSV format.
@@ -45,6 +47,14 @@ let averageAge = totalAge / dataObjects.length; console.log("Average Age:", aver
 
 
 // This code uses the reduce function to iterate over the dataObjects array and build the CSV string in a csv file. 
+
+// csv: This is the accumulator, which keeps accumulating the CSV string.
+
+// obj: This is the current object in the iteration.
+
+// csv +${obj.id},${obj.name},${obj.occupation},${obj.age}\n``: This concatenates the current object’s properties formatted as a CSV row to the accumulator string.
+
+// 'ID,Name,Occupation,Age\n': This is the initial value for the accumulator, representing the header row of the CSV.
 
 const csvContent = dataObjects.reduce((csv, obj) => 
     { return csv + `${obj.id},${obj.name},${obj.occupation},${obj.age}\n`; }, 'ID,Name,Occupation,Age\n');
